066e201d0c012cba606d842cb771734b79bcf9f2
1O16 <youiiyou1253@gmail.com>
2022-07-26 15:03:37 +0900

    Setting

---
+++ src/components/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import App from "./App";
+
+export default class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={App} />
+        </Switch>
+      </Router>
+    );
+  }
+}
