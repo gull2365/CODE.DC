1311f685d71c8d61c569a88c6debc772256cff9c
gull2365 <jych2365@gmail.com>
2022-07-27 21:14:50 +0900

    feat: 마진값 다시 맞추고 로그인/회원가입 콘텐츠에 맞춤

--- src/components/home.js
+++ src/components/home.js
@@ -27,18 +27,21 @@ const Header = () => {
         </div>
       </nav>
       <section>
-      <img src={CODEDC} alt="mainLogo" className="Mainlogo" />
-      <div className="Green_Circle">
-        <p className="GC_Text">2학기 신입 부원 모집!</p>
-      </div>
-      <h2 className="main_text">한세에서 개발자가 되고 싶다구요?</h2>
-      <h2 className="main_text2">그렇다면 CODE D.C.로 오세요!</h2>
-      <p className="main_text3">Code D.C. 는 한세사이버보안고등학교<br/> 
-         <b>유일</b> 개발 동아리로 한세 재학생이라면<br/>
-         <b>누구나</b> 지원하실 수 있습니다!</p>
-         <button className="main_btn">지원하기!</button>
+        <div className="Section_Container1">
+          <img src={CODEDC} alt="mainLogo" className="Mainlogo" />
+            <div className="Section_Container2">
+              <div className="Green_Circle">
+                <p className="GC_Text">2학기 신입 부원 모집!</p>
+              </div>
+              <h2 className="main_text">한세에서 개발자가 되고 싶다구요?</h2>
+              <h2 className="main_text2">그렇다면 CODE D.C.로 오세요!</h2>
+              <p className="main_text3">Code D.C. 는 한세사이버보안고등학교<br/> 
+                <b>유일</b> 개발 동아리로 한세 재학생이라면<br/>
+                <b>누구나</b> 지원하실 수 있습니다!</p>
+                <button className="main_btn">지원하기!</button>
+              </div>
+         </div>
       </section>
-
     </div>
   );
 };
