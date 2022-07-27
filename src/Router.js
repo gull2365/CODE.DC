50ef7347062c3d32bd6ad8edc7a7f29f1423e89c
gull2365 <jych2365@gmail.com>
2022-07-27 12:46:38 +0900

    router

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import App from "./App";
+import App from "./components/App";
 
 export default class Routes extends React.Component {
   render() {
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from '../pages/about';
+import Home from '../pages/home';
 
-function App() {
-  {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+};
 
 export default App;
