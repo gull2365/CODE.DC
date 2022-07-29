2f726bc0c70853fa44fdd701efcecc28eb020ef6
1O16 <youiiyou1253@gmail.com>
2022-07-29 13:24:15 +0900

    register, login, css

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,30 +1,39 @@
 import React, { useState } from "react";
-import Logo from '../img/Logo.png'
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import Logo from "../img/Logo.png";
+// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
 
   const onSubmit = (e) => {
     e.prevendDefault();
-  }
+  };
 
-  return ( 
-  <div>
-    <nav className="Header">
-      <a href="/">
-        <img src={Logo} alt="Logo" className="logo" />
-      </a>
-    </nav>
-    <h1 className="login">로그인</h1>
-    <form onSubmit={onSubmit} className="login_info">
-      <input type="text" name="email" required placeholder="이메일" />
-      <input type="password" name="password" required placeholder="비밀번호" />
-      <input type="submit" value="로그인" />
-    </form>
-  </div>
-  )
+  return (
+    <div>
+      <nav className="Header">
+        <a href="/">
+          <img src={Logo} alt="Logo" className="logo" />
+        </a>
+      </nav>
+      <h1 className="login">로그인</h1>
+      <form onSubmit={onSubmit} className="login_info">
+        <input type="text" name="email" required placeholder="이메일" />
+        <input
+          type="password"
+          name="password"
+          required
+          placeholder="비밀번호"
+        />
+        <input type="submit" value="로그인" />
+        <button className="toRegister">
+          Code D.C가 처음이신가요?
+          <a href="register">회원가입</a>
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,36 +1,73 @@
 import React, { useState } from "react";
-import Logo from '../img/Logo.png'
+import Logo from "../img/Logo.png";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
   const onSubmit = (e) => {
     e.prevendDefault();
 
     const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-  }
+    createUserWithEmailAndPassword(auth, email, password);
+  };
 
-  return   ( 
+  return (
     <div>
       <nav className="Header">
         <a href="/">
           <img src={Logo} alt="Logo" className="logo" />
         </a>
-        </nav>
+      </nav>
       <h1 className="login">회원가입</h1>
-        <form onSubmit={onSubmit} className="login_info">
-          <input type="text" name="email" required placeholder="이메일" />
-          <input type="password" name="password" required placeholder="비밀번호" />
-          <input type="number" name="phoneNumber" required placeholder="전화번호" />
-          <input type="text" name="hakbun" required placeholder="학번" />
-          <input type="text" name="name" required placeholder="이름" />
-          <input type="submit" value="회원가입" />
-        </form>
+      <form onSubmit={onSubmit} className="login_info">
+        <input
+          type="email"
+          name="email"
+          required
+          placeholder="이메일"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          name="password"
+          required
+          placeholder="비밀번호"
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="tel"
+          name="phoneNumber"
+          pattern="[0-9]{3}[0-9]{4}[0-9]{4}"
+          required
+          placeholder="전화번호"
+        />
+        <input
+          type="text"
+          name="hakbun"
+          pattern="[a-z]{1}[0-9]{4}"
+          required
+          placeholder="학번"
+        />
+        <input type="text" name="name" required placeholder="이름" />
+        <input type="submit" value="회원가입" />
+      </form>
     </div>
-    )
+  );
 };
 
 export default Register;
